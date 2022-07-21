36970f32125397c6ecc55a38a4ce9a5b9286d27c
Sara Negussie <snegussie@wesleyan.edu>
2022-07-21 17:12:03 -0400

    new way to query

--- SDSSquery.py
+++ SDSSquery.py
@@ -61,3 +61,36 @@ hdul = fits.open('NGC5548.fits')
 hdul.info()
 data = hdul[1].data
 """
+
+
+
+""""
+
+from astropy.io import fits
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.visualization import astropy_mpl_style
+from astropy.utils.data import get_pkg_data_filename
+from astropy.io import fits
+#plt.style.use(astropy_mpl_style)
+
+#print(np.size('NGC5548.fits'))
+#fits_image_filename = fits.util.get_testdata_filepath('NGC5548.fits')
+#image_file = get_pkg_data_filename('tutorials/FITS-images/HorseHead.fits')
+image_file = get_pkg_data_filename('NGC5548.fits')
+fits.info(image_file)
+image_data = fits.getdata(image_file, ext=0)
+print(image_data.shape)
+plt.figure()
+plt.imshow(image_data, cmap='gray')
+plt.colorbar()
+
+"""
+hdul = fits.open('NGC5548.fits')
+hdul.info()
+data = hdul[1].data
+"""
+
+
+
+"""
